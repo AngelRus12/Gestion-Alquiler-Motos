02a087bef5960a9978e142d75079e0f58681ae8a
AngelRus <angelruslatorre@gmail.com>
2026-04-18 12:08:14 +0200

    feat: Añadir el login local

--- loginbd.php
+++ loginbd.php
@@ -4,3 +4,11 @@ $db_database = "nntpktdd_alquilermotos"; // nombre exacto de la BD en cPanel
 $db_username = "nntpktdd_alquilermotos"; // usuario BD
 $db_password = "12345678";
 ?>
+<?php
+/*
+$db_hostname = "localhost"; // Se mantiene igual
+$db_database = "alquilermotos"; // O el nombre exacto que le hayas dado en tu phpMyAdmin local
+$db_username = "root"; // Usuario por defecto de WampServer
+$db_password = ""; // Por defecto, WampServer no tiene contraseña
+*/
+?>
